1352bc1b8d514559b4ee0449a846bf1dade0be5c
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-10-13 23:27:03 +0700

    revert fix security

--- src/main/java/com/app/toeic/config/WebSecurityConfig.java
+++ src/main/java/com/app/toeic/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/user/**").permitAll()
                         .requestMatchers("/admin/**").hasAuthority("ADMIN")
                         .requestMatchers("/superadmin/**").hasAuthority("SUPERADMIN")
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 );
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
