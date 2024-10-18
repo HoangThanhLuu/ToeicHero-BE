07a0299f7d83631ab69064869a5d197605e474f0
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-10-18 23:53:54 +0700

    update crawl data for exam (part 1,2,3,4)

--- src/main/java/com/app/toeic/crawl/payload/CrawlDTO.java
+++ src/main/java/com/app/toeic/crawl/payload/CrawlDTO.java
@@ -1,14 +1,16 @@
-package com.app.toeic.external.payload;
+package com.app.toeic.crawl.payload;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CrawlDTO {
     String url;
     String providerCode;
-    String agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+    String agent = "";
 }
