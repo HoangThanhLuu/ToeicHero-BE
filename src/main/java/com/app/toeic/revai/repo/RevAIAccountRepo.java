7f395d639b547548df0ec500d86c65569c63f754
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-10-21 18:05:04 +0700

    fix all response with follow standard format

--- src/main/java/com/app/toeic/revai/repo/RevAIAccountRepo.java
+++ src/main/java/com/app/toeic/revai/repo/RevAIAccountRepo.java
@@ -4,7 +4,9 @@ import com.app.toeic.revai.model.RevAIAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RevAIAccountDTO extends JpaRepository<RevAIAccount, Long>{
+import java.util.Optional;
 
+@Repository
+public interface RevAIAccountRepo extends JpaRepository<RevAIAccount, Integer>{
+    Optional<RevAIAccount> findByEmail(String email);
 }
